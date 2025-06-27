52c0cf57d0729afe142d69dfd3504ed52ad6bddd
AngelOrtizz <adlahiangel1921@gmail.com>
2025-06-26 20:53:41 -0600

    update login

---
+++ src/material.module.ts
@@ -0,0 +1,24 @@
+import { NgModule } from  "@angular/core";
+import {MatButtonModule} from '@angular/material/button';
+import {MatToolbarModule} from '@angular/material/toolbar';
+import {MatIconModule} from '@angular/material/icon';
+import {MatCardModule} from '@angular/material/card';
+import {MatInputModule} from '@angular/material/input';
+//import {MatInputModule} from '@angular/material/input';
+
+
+
+
+const myModules: any = [
+    MatButtonModule,
+    MatToolbarModule,
+    MatIconModule,
+    MatCardModule,
+    MatInputModule,
+
+];
+@NgModule({
+    imports: [...myModules],
+    exports: [...myModules]
+})
+export class MaterialModule {}
