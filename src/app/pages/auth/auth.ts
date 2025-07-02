b3b1eb40f340b2043a13aba15e2a0702b8a45a1e
AngelOrtizz <adlahiangel1921@gmail.com>
2025-07-02 17:48:53 -0600

    logica-Form

--- src/app/pages/auth/auth.ts
+++ src/app/pages/auth/auth.ts
@@ -1,4 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { FormBuilder, Validators } from '@angular/forms';
+import { Subject } from 'rxjs';
+import { BaseForm } from '../../shared/utils/base.form';
 
 @Component({
   selector: 'app-auth',
@@ -6,6 +9,23 @@ import { Component } from '@angular/core';
   templateUrl: './auth.html',
   styleUrl: './auth.scss'
 })
-export class Auth {
+export class Auth implements OnInit, OnDestroy {
+  hide = true;
 
+  loginForm;
+
+  constructor (private fb:FormBuilder, public baseForm: BaseForm){
+    this.loginForm = this.fb.group({
+      username: ['',[Validators.required, Validators.minLength(3)]],
+      password: ['',[Validators.required, Validators.minLength(3)]]
+    });
+    console.log("init constructor")
+  }
+  ngOnInit(): void {
+    console.log("init Oninit")
+  }
+
+  ngOnDestroy(): void {
+    console.log("destroy")
+  }
 }
---
+++ src/app/shared/utils/base.form.ts
@@ -0,0 +1,36 @@
+import { Injectable } from "@angular/core";
+import { AbstractControl } from "@angular/forms";
+
+@Injectable({ providedIn: 'root'})
+export class BaseForm{
+  constructor(){}
+
+  isInvalidField(form: AbstractControl | null ): boolean{
+    var bandera = false;
+    if (form){
+      bandera = form.touched || form.dirty && form.invalid; 
+    }
+    return bandera;
+  }
+
+  getErrorMessage(form: AbstractControl | null ){
+    let message = '';
+    if (form){
+      const { errors } = form;
+      
+      if (errors){
+          const messages: any = {
+            required: "Campo requerido",
+            email: "Formato invalido",
+            pattern: "Formato invalido",
+            min: "El rango no es correcto",
+            max: "El rango no es correcto",
+            minlength: "Demasiado corto"
+          }
+          const errorkey = Object.keys(errors).find(Boolean);
+          if (errorkey) message = messages[errorkey];
+      }
+    }
+    return message;
+  }
+}
